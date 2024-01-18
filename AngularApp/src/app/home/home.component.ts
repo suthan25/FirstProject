06d28497df7c93a8292fe98efe50244dadb87a21
suthan25 <suthan25052000@gmail.com>
2024-01-18 11:54:23 +0530

    http params

--- AngularApp/src/app/home/home.component.ts
+++ AngularApp/src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
-
+export class HomeComponent{
+  input!: number;
+  constructor(private http:HttpClient){}
+  search(term:number){
+    const options = term?{params:new HttpParams().set('userId',term)}:{}
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/todos/',options).subscribe((x:any)=>console.log(x))
+  }
 }
